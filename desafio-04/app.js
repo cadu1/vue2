20e78c792ab00f707b96b2465401894833b76c9e
Carlos Oliveira <carlos.oliveira@tray.net.br>
2019-01-16 13:52:09 -0200

    Started Challenge 4

---
+++ desafio-04/app.js
@@ -0,0 +1,15 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		addEfeito: false
+	},
+	methods: {
+		iniciarEfeito() {
+			this.addEfeito = !this.addEfeito;
+		},
+		iniciarProgresso() {
+
+		}
+	},
+
+})
