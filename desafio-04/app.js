d3f2a145b3259a4e332affa38d01db0e79d0c58e
Carlos Oliveira <carlos.oliveira@tray.net.br>
2019-01-17 14:05:59 -0200

    Challenge 5 finished

--- desafio-04/app.js
+++ desafio-04/app.js
@@ -1,15 +1,44 @@
 new Vue({
 	el: '#desafio',
 	data: {
-		addEfeito: false
+		addEfeito: "destaque",
+		aplicaCss: "encolher",
+		perigo: false,
+		aplicaStyle: {
+			width: '100px',
+			height: '100px'
+		},
+		contador: 0,
+		largura: "",
+		cor: ""
 	},
 	methods: {
 		iniciarEfeito() {
-			this.addEfeito = !this.addEfeito;
+			setInterval(() => {
+				this.addEfeito = !this.addEfeito;
+			}, 1000);
 		},
 		iniciarProgresso() {
-
+			let thread = setInterval(()=>{
+				this.contador++;
+				this.largura = this.progresso;
+				console.log(this.largura);
+				if(this.contador === 10) {
+					clearInterval(thread);
+				}
+			}, 500);
+		},
+		setPerigo(event) {
+			if(event.target.value === "true") {
+				this.perigo = true;
+			} else if(event.target.value === "false") {
+				this.perigo = false;
+			}
 		}
 	},
-
+	computed: {
+		progresso() {
+			return 'width:' + (this.contador*40) + 'px';
+		}
+	}
 })
